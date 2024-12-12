015d82ade36f2ca5907ad8f3f236915a1ec5ac69
akito-shine <contact@orsontechnology.com>
2024-12-12 11:58:36 +0700

    aicoach_vapi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Vapi from "@vapi-ai/web";
 import { isPublicKeyMissingError } from "./utils";
 
 // Put your Vapi Public Key below.
-const vapi = new Vapi("0000XXXX-XXXX-XXXX-XXXX-XXXXXXXX0000");
+const vapi = new Vapi("170f0524-de27-4090-adf9-8f13e3020ba5");
 
 const App = () => {
   const [connecting, setConnecting] = useState(false);
